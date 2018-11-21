bacbd693af9008ca5cfc189bd9bd68b3263a4202
12masta <tVW-q8o-Qnn-SSb>
2018-11-21 23:59:31 +0100

    create or update implemented

--- src/main/java/com/example/activeCampaignApiSpringBoot/DemoApplication.java
+++ src/main/java/com/example/activeCampaignApiSpringBoot/DemoApplication.java
@@ -1,12 +1,20 @@
 package com.example.activeCampaignApiSpringBoot;
 
+import com.example.activeCampaignApiSpringBoot.convert.JsonConverter;
+import com.example.activeCampaignApiSpringBoot.http.Http;
+import com.example.activeCampaignApiSpringBoot.model.contact.ContactContainer;
+import com.example.activeCampaignApiSpringBoot.model.response.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.io.IOException;
+
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
@@ -15,6 +23,23 @@ public class DemoApplication {
 	String home() {
 		return "Hello World!";
 	}
+
+	@PostMapping("/createorupdatecontact")
+	@ResponseBody
+	Response createorupdatecontact(@RequestBody ContactContainer contact) {
+		Http http = new Http();
+		Response response =  new Response(200, "OK");
+		try {
+			okhttp3.Response okHttpResponse = http.post("https://marcinstanek.api-us1.com/api/3/contact/sync", JsonConverter.toJsonString(contact));
+			response.setResponse(okHttpResponse.message().toString());
+			response.setStatusCode(okHttpResponse.code());
+		} catch (IOException e) {
+			response.setStatusCode(500);
+			response.setResponse(e.getMessage());
+		}
+		return response;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
