9706da1a85ba27feae73375ea92e9962a3218778
12masta <tVW-q8o-Qnn-SSb>
2018-11-22 00:47:47 +0100

    cross orgin update

--- src/main/java/com/example/activeCampaignApiSpringBoot/DemoApplication.java
+++ src/main/java/com/example/activeCampaignApiSpringBoot/DemoApplication.java
@@ -6,9 +6,7 @@ import com.example.activeCampaignApiSpringBoot.model.contact.ContactContainer;
 import com.example.activeCampaignApiSpringBoot.model.response.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,36 +19,30 @@ import java.io.IOException;
 @SpringBootApplication
 public class DemoApplication {
 
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello World!";
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
-	@PostMapping("/createorupdatecontact")
-	@ResponseBody
-	ResponseEntity createorupdatecontact(@RequestBody ContactContainer contact) {
-		Http http = new Http();
-		Response response =  new Response(200, "OK");
-		try {
-			okhttp3.Response okHttpResponse = http.post("https://marcinstanek.api-us1.com/api/3/contact/sync", JsonConverter.toJsonString(contact));
-			response.setResponse(okHttpResponse.message().toString());
-			response.setStatusCode(okHttpResponse.code());
-		} catch (IOException e) {
-			response.setStatusCode(500);
-			response.setResponse(e.getMessage());
-		}
-		HttpHeaders responseHeaders = new HttpHeaders();
-		responseHeaders.set("Access-Control-Allow-Origin", "https://kurstestowania.pl");
-		responseHeaders.set("Access-Control-Allow-Origin", "https://www.kurstestowania.pl");
-		responseHeaders.set("Access-Control-Allow-Origin", "*");
-		responseHeaders.set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
-		responseHeaders.set("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token");
-		return new ResponseEntity<String>(response.getResponse(), responseHeaders, HttpStatus.CREATED);
+    @PostMapping("/createorupdatecontact")
+    @CrossOrigin
+    @ResponseBody
+    Response createorupdatecontact(@RequestBody ContactContainer contact) {
+        Http http = new Http();
+        Response response = new Response(200, "OK");
+        try {
+            okhttp3.Response okHttpResponse = http.post("https://marcinstanek.api-us1.com/api/3/contact/sync", JsonConverter.toJsonString(contact));
+            response.setResponse(okHttpResponse.message().toString());
+            response.setStatusCode(okHttpResponse.code());
+        } catch (IOException e) {
+            response.setStatusCode(500);
+            response.setResponse(e.getMessage());
+        }
+        return response;
+    }
 
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
